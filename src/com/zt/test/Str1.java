e3f3a57da43640dc57d0b1fb77e0f186b12a57cb
scjdegit <64897486+scjdegit@users.noreply.github.com>
2020-05-06 19:22:53 +0800

    Update Str1.java

--- src/com/zt/test/Str1.java
+++ src/com/zt/test/Str1.java
@@ -5,7 +5,7 @@ public class Str1 {
    public static void main(String []args){
      System.out.println("helle word");
      int a=10;
-     int b=20;
+     int b=5;
      System.out.print(a+b);
 
     }

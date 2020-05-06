776e0d9345675bc64699b161c9c4be75c56ef4ae
asus <1653533573@qq.com>
2020-05-06 19:17:48 +0800

    Initial commit

---
+++ src/com/zt/test/Str1.java
@@ -0,0 +1,13 @@
+package com.zt.test;
+
+public class Str1 {
+    private String name;
+   public static void main(String []args){
+     System.out.println("helle word");
+     int a=10;
+     int b=20;
+     System.out.print(a+b);
+
+    }
+
+}
